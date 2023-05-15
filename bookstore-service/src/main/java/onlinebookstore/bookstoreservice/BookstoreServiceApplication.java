5316bd9a2f86f6eb84b914c52e40e785009b5c6d
nguyenlam <sweetloveinyourheart@gmail.com>
2023-05-15 11:56:27 +0700

    init server

---
+++ bookstore-service/src/main/java/onlinebookstore/bookstoreservice/BookstoreServiceApplication.java
@@ -0,0 +1,13 @@
+package onlinebookstore.bookstoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreServiceApplication.class, args);
+	}
+
+}
---
+++ bookstore-service/src/test/java/onlinebookstore/bookstoreservice/BookstoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package onlinebookstore.bookstoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
