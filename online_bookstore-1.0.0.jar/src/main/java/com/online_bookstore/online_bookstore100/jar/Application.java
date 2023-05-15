7ca09f44beca4ce8b2f84bea63d8bcd4ba289711
Nguyen Lam <nguyenlam@MacBook-Pro-14.local>
2023-05-15 10:46:27 +0700

    init spring server

---
+++ online_bookstore-1.0.0.jar/src/main/java/com/online_bookstore/online_bookstore100/jar/Application.java
@@ -0,0 +1,13 @@
+package com.online_bookstore.online_bookstore100.jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ online_bookstore-1.0.0.jar/src/test/java/com/online_bookstore/online_bookstore100/jar/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_bookstore.online_bookstore100.jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
